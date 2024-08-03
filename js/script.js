2d0700b255bb0e8673a6cc5e5b8083706a1df073
adahyh <138656518+adahyh@users.noreply.github.com>
2024-08-03 02:08:09 -0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,37 +1,27 @@
 const homeLogo = document.getElementById("logoImage");
 
-homeLogo.addEventListener('click', function() {
-  // Redirect to another HTML page
-  window.location.href = 'index.html';
+homeLogo.addEventListener("click", function() {
+  
+  window.location.href = "index.html";
 });
 
 
-
-
-
-// Select the div element
-const clickProject = document.getElementById('menolearn');
-
-// Add a click event listener to the div
-clickProject.addEventListener('click', function() {
-  // Redirect to another HTML page
-  window.location.href = 'menolearn.html';
+// click to open menolearn
+document.getElementById("menolearn").addEventListener("click", function() {
+  window.location.href = "menolearn.html";
 });
-
-// const clickTicket = document.getElementById('tickettasker');
-
-
-// clickTicket.addEventListener('click', function() {
-
-//   window.location.href = 'tickettasker.html';
-// });
 
 // click to open tickettasker
-document.getElementById('tickettasker').addEventListener('click', function() {
-  window.location.href = 'tickettasker.html';
+document.getElementById("tickettasker").addEventListener("click", function() {
+  window.location.href = "tickettasker.html";
 });
 
-document.getElementById('trolley').addEventListener('click', function() {
-  window.location.href = 'trolley.html';
+// click to open trolley
+document.getElementById("trolley").addEventListener("click", function() {
+  window.location.href = "trolley.html";
 });
 
+// click to open trolley
+document.getElementById("mobileorder").addEventListener("click", function() {
+  window.location.href = "mobileorder.html";
+});
